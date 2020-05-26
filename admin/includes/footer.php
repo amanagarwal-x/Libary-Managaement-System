6883a26bf1be8b9fe30487ea569c87629ea9f5ec
aman <aman.agarwal150@gmail.com>
2020-05-26 12:17:38 +0530

    Added Project

---
+++ admin/includes/footer.php
@@ -0,0 +1,15 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-1">
+                   <img src="assets/img/mit_icon.png" style="width: 15vw; min-width: 100px;"/>
+                </div>
+                
+                <div class="col-md-12">
+                   &copy; 2020 Online Library Management System |<a href="https://www.linkedin.com/in/aman-agarwal-ba7946175/" target="_blank"  > Designed by : Aman Agarwal</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
